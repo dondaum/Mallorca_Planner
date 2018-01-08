4a3fe1508dda34e2994cba3fc790ed7382ceacc9
dondaum <sebastian.daum89@gmail.com>
2018-01-08 19:41:51 +0100

    deleted temp images and added remove logic und rspec after tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -42,6 +42,13 @@ RSpec.configure do |config|
     mocks.verify_partial_doubles = true
   end
 
+  config.after(:all) do
+    # Get rid of the linked images
+    if Rails.env.test? || Rails.env.cucumber?
+      FileUtils.rm_rf(Dir["#{Rails.root}/public/uploads/temp/[^.]*"])
+    end
+  end
+
   # This option will default to `:apply_to_host_groups` in RSpec 4 (and will
   # have no way to turn it off -- the option exists only for backwards
   # compatibility in RSpec 3). It causes shared context metadata to be
