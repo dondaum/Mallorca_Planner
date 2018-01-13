a04eec43ea35a1129139bdb71fe63b585e2c92f0
dondaum <sebastian.daum89@gmail.com>
2018-01-13 17:18:06 +0100

    changed redis url to env url

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,15 +1,15 @@
 if Rails.env.production?
 
   Sidekiq.configure_server do |config|
-    config.redis = { url: Figaro.env.heroku_redis_url  }
+    config.redis = { url: ENV['REDIS_PROVIDER']  }
   end
 
   Sidekiq.configure_client do |config|
-    config.redis = { url: Figaro.env.heroku_redis_url  }
+    config.redis = { url: ENV['REDIS_PROVIDER']  }
   end
 
 else
-  
+
   Sidekiq.configure_server do |config|
     config.redis = { url: 'redis://localhost:6379/0'  }
   end
